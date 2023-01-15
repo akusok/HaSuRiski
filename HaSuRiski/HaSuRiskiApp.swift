1414adeb167757e03014b62dc048e02690e4fca9
Anton Akusok <akusok.a@gmail.com>
2023-01-15 13:09:21 +0200

    Initial Commit

---
+++ HaSuRiski/HaSuRiskiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaSuRiskiApp.swift
+//  HaSuRiski
+//
+//  Created by Anton on 15.1.2023.
+//
+
+import SwiftUI
+
+@main
+struct HaSuRiskiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
