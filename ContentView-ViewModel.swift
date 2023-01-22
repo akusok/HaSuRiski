9db7f87270453fbe1d2c030426196e37a11f7668
Anton Akusok <akusok.a@gmail.com>
2023-01-22 11:40:44 +0200

    Start with MVVM

---
+++ ContentView-ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView-ViewModel.swift
+//  HaSuRiski
+//
+//  Created by Anton Akusok on 22.01.23.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    
+    // tab this class inside our ContentView
+    // UI updates must happen on the @MainActor
+    @MainActor class ViewMode: ObservableObject {
+        
+    }
+}
