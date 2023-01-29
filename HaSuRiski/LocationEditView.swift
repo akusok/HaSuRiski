51dc5eaa9a7e4225d5420a006484742465bbfeb8
Anton Akusok <akusok.a@gmail.com>
2023-01-29 23:21:49 +0200

    Remove Wikipedia link load

--- HaSuRiski/LocationEditView.swift
+++ HaSuRiski/LocationEditView.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct LocationEditView: View {
-    enum LoadingState {
-        case loading, loaded, failed
-    }
     
     @Environment(\.dismiss) var dismiss
     var location: Location
@@ -18,14 +15,7 @@ struct LocationEditView: View {
 
     @State private var name: String
     @State private var acidSulfate: Bool
-    
-    // default state is "currently loading data"
-    @State private var loadingState = LoadingState.loading
-    
-    // we don't care about query or result in the SwiftUI layout
-    // only storing the array of actual pages coming back
-    @State private var pages = [Page]()
-        
+            
     var body: some View {
         NavigationView {
             Form {
@@ -34,23 +24,6 @@ struct LocationEditView: View {
                     VStack {
                         Toggle(acidSulfate ? "Acid Sulfate soil" : "Normal Soil", isOn: $acidSulfate)
                             .foregroundColor(acidSulfate ? .red : .green)
-                    }
-                }
-                Section("Nearby...") {
-                    switch loadingState {
-                    case .loading:
-                        Text("Loading...")
-                    case .loaded:
-                        // have Wikipedia data back, time to show it on the screen
-                        ForEach(pages, id: \.pageid) { page in
-                            Text(page.title)
-                                .font(.headline)
-                            + Text(": ")
-                            + Text(page.description)
-                                .italic()
-                        }
-                    case .failed:
-                        Text("Please try again later.")
                     }
                 }
             }
@@ -66,9 +39,6 @@ struct LocationEditView: View {
                     dismiss()
                 }
             }
-            .task {
-                await fetchNearbyPlaces()
-            }
         }
     }
     
@@ -80,25 +50,6 @@ struct LocationEditView: View {
         _name = State(initialValue: location.name)
         _acidSulfate = State(initialValue: location.acidSulfate)
     }
-    
-    func fetchNearbyPlaces() async {
-        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.coordinate.latitude)%7C\(location.coordinate.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
-        
-        guard let url = URL(string: urlString) else {
-            print("Bad URL: \(urlString)")
-            return
-        }
-        
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let returnedItems = try JSONDecoder().decode(Result.self, from: data)
-            pages = returnedItems.query.pages.values.sorted()
-            loadingState = .loaded
-        } catch {
-            loadingState = .failed
-        }
-    }
-    
 }
 
 struct AnnotationEditView_Previews: PreviewProvider {
