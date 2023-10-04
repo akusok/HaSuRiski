03bf2ccc23d0a455b4f590e344096f92d2570972
Anton Akusok <akusok.a@gmail.com>
2023-10-05 00:49:33 +0300

    Updated model data

--- MapKitTest/Model/ELMModel.swift
+++ MapKitTest/Model/ELMModel.swift
@@ -20,22 +20,22 @@ class ELMModel {
         // use uploaded files
         let docDir = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
         let allFiles = try! FileManager.default.contentsOfDirectory(at: docDir, includingPropertiesForKeys: nil)
-        let myFiles = Dictionary(uniqueKeysWithValues: allFiles.map { ($0.lastPathComponent, $0) })
+//        let myFiles = Dictionary(uniqueKeysWithValues: allFiles.map { ($0.lastPathComponent, $0) })
         
         let bK = 1  // weight batches
-        let bL = 150
+        let bL = 100
         
         print("Data file:")
-        let fileX = mainBundle.url(forResource: "hX", withExtension: "npy")!
-        let fileY = mainBundle.url(forResource: "hY", withExtension: "npy")!
-        let fileW = mainBundle.url(forResource: "hW_150", withExtension: "npy")!
-        let fileBias = mainBundle.url(forResource: "hbias_150", withExtension: "npy")!
+        let fileX = mainBundle.url(forResource: "X", withExtension: "npy")!
+        let fileY = mainBundle.url(forResource: "Y", withExtension: "npy")!
+        let fileW = mainBundle.url(forResource: "W_\(bL)", withExtension: "npy")!
+        let fileBias = mainBundle.url(forResource: "bias_\(bL)", withExtension: "npy")!
 
         let X: MPSMatrix = loadFromNpy(contentsOf: fileX, device: self.device)
         let Y: MPSMatrix = loadFromNpy(contentsOf: fileY, device: self.device)
         
         let t0 = CFAbsoluteTimeGetCurrent()
-        self.model = ELM(device: self.device, bK: bK, bL: bL, alpha: 1E1, W: [fileW], bias: [fileBias])
+        self.model = ELM(device: self.device, bK: bK, bL: bL, alpha: 1E2, W: [fileW], bias: [fileBias])
         self.model!.fit(X: X, Y: Y)
         let t = CFAbsoluteTimeGetCurrent() - t0
         print(String(format: "Training time: %.3f", t))
